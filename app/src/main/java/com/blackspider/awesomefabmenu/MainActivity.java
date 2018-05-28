9f82dfd7d86168e316f4cbe9a4b33e37135d4576
hasnain@w3engineers.com <hasnain@w3engineers.com>
2018-05-28 17:01:49 +0600

    Initial commit

---
+++ app/src/main/java/com/blackspider/awesomefabmenu/MainActivity.java
@@ -0,0 +1,255 @@
+package com.blackspider.awesomefabmenu;
+
+import android.animation.Animator;
+import android.animation.AnimatorListenerAdapter;
+import android.animation.AnimatorSet;
+import android.animation.ObjectAnimator;
+import android.content.Context;
+import android.content.Intent;
+import android.os.Build;
+import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
+import android.support.v7.app.ActionBar;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.View;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.ViewAnimationUtils;
+import android.view.animation.AccelerateDecelerateInterpolator;
+import android.view.animation.AnticipateInterpolator;
+import android.view.animation.OvershootInterpolator;
+import android.widget.Button;
+import android.widget.Toast;
+
+import com.blackspider.util.helper.AnimatorUtils;
+import com.blackspider.util.lib.archlayout.ArcLayout;
+import com.blackspider.util.widget.ClipRevealFrame;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class MainActivity extends AppCompatActivity implements View.OnClickListener{
+
+    private static final String KEY_DEMO = "demo";
+    Toast toast = null;
+    FloatingActionButton fab;
+    Button btnShoutout, btnAsk, btnPoll;
+    View rootLayout;
+    ClipRevealFrame menuLayout;
+    ArcLayout arcLayout;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        Toolbar toolbar = findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+        fab = findViewById(R.id.fab);
+        btnShoutout = findViewById(R.id.btn_shoutout);
+        btnAsk = findViewById(R.id.btn_ask);
+        btnPoll = findViewById(R.id.btn_poll);
+        menuLayout = findViewById(R.id.menu_layout);
+        rootLayout = findViewById(R.id.root_layout);
+        arcLayout = (ArcLayout) findViewById(R.id.arc_layout);
+
+        btnShoutout.setOnClickListener(this);
+        btnAsk.setOnClickListener(this);
+        btnPoll.setOnClickListener(this);
+        fab.setOnClickListener(this);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        if (item.getItemId() == android.R.id.home) {
+            finish();
+            return true;
+        }
+        return super.onOptionsItemSelected(item);
+    }
+
+    @Override
+    public void onClick(View v) {
+        switch (v.getId()){
+            case R.id.fab:
+                onFabClick(v);
+                return;
+
+            case R.id.btn_shoutout:
+                showToast("Shoutout");
+                return;
+
+            case R.id.btn_ask:
+                showToast("Ask question");
+                return;
+
+            case R.id.btn_poll:
+                showToast("Create poll");
+                return;
+        }
+
+    }
+
+    private void showToast(String msg) {
+        if (toast != null) {
+            toast.cancel();
+        }
+        toast = Toast.makeText(this, msg, Toast.LENGTH_SHORT);
+        toast.show();
+
+    }
+
+    private void onFabClick(View v) {
+        int x = (v.getLeft() + v.getRight()) / 2;
+        int y = (v.getTop() + v.getBottom()) / 2;
+        float radiusOfFab = 1f * v.getWidth() / 2f;
+        float radiusFromFabToRoot = (float) Math.hypot(
+                Math.max(x, rootLayout.getWidth() - x),
+                Math.max(y, rootLayout.getHeight() - y));
+
+        if (v.isSelected()) {
+            hideMenu(x, y, radiusFromFabToRoot, radiusOfFab);
+        } else {
+            showMenu(x, y, radiusOfFab, radiusFromFabToRoot);
+        }
+
+        v.setSelected(!v.isSelected());
+    }
+
+    @SuppressWarnings("NewApi")
+    private void showMenu(int cx, int cy, float startRadius, float endRadius) {
+        menuLayout.setVisibility(View.VISIBLE);
+
+        List<Animator> animList = new ArrayList<>();
+
+        Animator revealAnim = createCircularReveal(menuLayout, cx, cy, startRadius, endRadius);
+        revealAnim.setInterpolator(new AccelerateDecelerateInterpolator());
+        //revealAnim.setDuration(400);
+        animList.add(revealAnim);
+
+        for (int i = 0, len = arcLayout.getChildCount(); i < len; i++) {
+            animList.add(createShowItemAnimator(arcLayout.getChildAt(i)));
+        }
+
+        AnimatorSet animSet = new AnimatorSet();
+        animSet.setDuration(800);
+        animSet.setInterpolator(new OvershootInterpolator());
+        animSet.playTogether(animList);
+        animSet.start();
+    }
+
+    @SuppressWarnings("NewApi")
+    private void hideMenu(int cx, int cy, float startRadius, float endRadius) {
+
+        List<Animator> animList = new ArrayList<>();
+
+        for (int i = arcLayout.getChildCount() - 1; i >= 0; i--) {
+            animList.add(createHideItemAnimator(arcLayout.getChildAt(i)));
+        }
+
+        Animator revealAnim = createCircularReveal(menuLayout, cx, cy, startRadius, endRadius);
+        revealAnim.setInterpolator(new AccelerateDecelerateInterpolator());
+        //revealAnim.setDuration(400);
+        revealAnim.addListener(new AnimatorListenerAdapter() {
+            @Override
+            public void onAnimationEnd(Animator animation) {
+                super.onAnimationEnd(animation);
+                menuLayout.setVisibility(View.INVISIBLE);
+            }
+        });
+
+        animList.add(revealAnim);
+
+        AnimatorSet animSet = new AnimatorSet();
+        animSet.setDuration(800);
+        animSet.setInterpolator(new AnticipateInterpolator());
+        animSet.playTogether(animList);
+        animSet.addListener(new AnimatorListenerAdapter() {
+            @Override
+            public void onAnimationEnd(Animator animation) {
+                super.onAnimationEnd(animation);
+                menuLayout.setVisibility(View.INVISIBLE);
+            }
+        });
+        animSet.start();
+
+    }
+
+    private Animator createShowItemAnimator(View item) {
+
+        float dx = fab.getX() - item.getX();
+        float dy = fab.getY() - item.getY();
+
+        item.setRotation(0f);
+        item.setTranslationX(dx);
+        item.setTranslationY(dy);
+
+        Animator anim = ObjectAnimator.ofPropertyValuesHolder(
+                item,
+                AnimatorUtils.rotation(0f, 360f),
+                AnimatorUtils.translationX(dx, 0f),
+                AnimatorUtils.translationY(dy, 0f)
+        );
+
+        return anim;
+    }
+
+    private Animator createHideItemAnimator(final View item) {
+        float dx = fab.getX() - item.getX();
+        float dy = fab.getY() - item.getY();
+
+        Animator anim = ObjectAnimator.ofPropertyValuesHolder(
+                item,
+                AnimatorUtils.rotation(360f, 0f),
+                AnimatorUtils.translationX(0f, dx),
+                AnimatorUtils.translationY(0f, dy)
+        );
+
+        anim.addListener(new AnimatorListenerAdapter() {
+            @Override
+            public void onAnimationEnd(Animator animation) {
+                super.onAnimationEnd(animation);
+                item.setTranslationX(0f);
+                item.setTranslationY(0f);
+            }
+        });
+
+        return anim;
+    }
+
+    private Animator createCircularReveal(final ClipRevealFrame view, int x, int y, float startRadius,
+                                          float endRadius) {
+        final Animator reveal;
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) {
+            reveal = ViewAnimationUtils.createCircularReveal(view, x, y, startRadius, endRadius);
+        } else {
+            view.setClipOutLines(true);
+            view.setClipCenter(x, y);
+            reveal = ObjectAnimator.ofFloat(view, "ClipRadius", startRadius, endRadius);
+            reveal.addListener(new Animator.AnimatorListener() {
+                @Override
+                public void onAnimationStart(Animator animation) {
+
+                }
+
+                @Override
+                public void onAnimationEnd(Animator animation) {
+                    view.setClipOutLines(false);
+                }
+
+                @Override
+                public void onAnimationCancel(Animator animation) {
+
+                }
+
+                @Override
+                public void onAnimationRepeat(Animator animation) {
+
+                }
+            });
+        }
+        return reveal;
+    }
+}
